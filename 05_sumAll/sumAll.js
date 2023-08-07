6c38a4b2241d557b2dbf382f26f5c5b3dc552be4
sbaginski <s.baginski@student.uw.edu.pl>
2023-08-07 21:43:29 +0200

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (a < 0 || b < 0) {
+    return 'ERROR';
+  }
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return 'ERROR';
+  }
 
+  const lower = a <= b ? a : b;
+  const upper = a > b ? a : b;
+  let sum = 0;
+  for (let i = lower; i <= upper; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line

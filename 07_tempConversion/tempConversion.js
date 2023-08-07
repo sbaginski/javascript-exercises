8b6e0220e89f0b64d7a39bb4ec8e044a68bcc888
sbaginski <s.baginski@student.uw.edu.pl>
2023-08-07 21:53:59 +0200

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const converted = (temp - 32) * 5 / 9;
+  return Math.round(10 * converted) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const converted = (temp * 9 / 5 + 32);
+  return Math.round(10 * converted) / 10;
 };
 
 // Do not edit below this line

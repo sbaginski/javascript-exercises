cad3755b5ed188d495865835dc3c31afc2cdc083
sbaginski <s.baginski@student.uw.edu.pl>
2023-08-08 18:04:10 +0200

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, current) => accumulator + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, current) => accumulator * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a < 0) {
+    return 'ERROR';
+  }
+  if (a === 0) {
+    return 1;
+  }
+
+	let output = 1;
+  for (let i = 1; i <= a; i++) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line

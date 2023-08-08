bd425c58ebc3e4e3db790bae905e55e4dd397e93
sbaginski <s.baginski@student.uw.edu.pl>
2023-08-08 19:18:49 +0200

    Complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const today = new Date();
+  let oldest = {
+    name: people[0].name,
+    age: 0
+  };
+  for (person of people) {
+    let death = 'yearOfDeath' in person 
+      ? person.yearOfDeath : today.getFullYear();
+    let age = death - person.yearOfBirth;
+    if (age >= oldest.age) {
+      oldest.name = person.name;
+      oldest.age = age;
+    }
+  }
+  for (person of people) {
+    if (person.name === oldest.name) {
+      return person;
+    }
+  }
 };
 
 // Do not edit below this line

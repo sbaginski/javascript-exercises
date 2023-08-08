1565cef248d371cc7cb9a3aed03c7bcafd835093
sbaginski <s.baginski@student.uw.edu.pl>
2023-08-08 18:52:28 +0200

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const parsed = string.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '');
+  let reverse = '';
+  for (let i = parsed.length - 1; i >= 0; i--) {
+    reverse = reverse.concat(parsed.charAt(i));
+  }
+  return reverse === parsed;
 };
 
 // Do not edit below this line
